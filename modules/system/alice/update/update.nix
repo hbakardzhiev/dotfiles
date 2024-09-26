3ddc0108966edb23784f3584d430ea759e9f040a
hbakardzhiev <h.bakardzhiev@gmx.com>
2024-09-26 22:35:01 +0200

    [updateComplete]

--- modules/system/alice/update/update.nix
+++ modules/system/alice/update/update.nix
@@ -3,7 +3,7 @@
   systemd.services.pull-updates = {
     description = "Pulls changes to system config";
     restartIfChanged = false;
-    onSuccess = [ "rebuild.service" ];
+    onSuccess = [ "nixos-upgrade.service" ];
     startAt = "04:40";
     path = [pkgs.git pkgs.openssh];
     script = ''
@@ -16,24 +16,38 @@
       Type = "oneshot";
     };
   };
+  system.autoUpgrade = {
+    enable = true;
+    flake = "git+file:///etc/nixos";
+    flags = [
+      # "--update-input"
+      # "nixpkgs"
+      "--no-write-lock-file"
+      "-L" # print build logs
+    ];
+    dates = "05:00";
+    allowReboot = true;
+    randomizedDelaySec = "45min";
+  };
+
         # hostname="$(hostname -s)"
         # --flake /etc/nixos/.#"''${hostname}"
 
-  systemd.services.rebuild = {
-    description = "Rebuilds and activates system config";
-    restartIfChanged = false;
-    path = [ pkgs.nixos-rebuild pkgs.systemd pkgs.nettools pkgs.git ];
-    script = ''
-      nixos-rebuild boot 
-      booted="$(readlink /run/booted-system/{initrd,kernel,kernel-modules})"
-      built="$(readlink /nix/var/nix/profiles/system/{initrd,kernel,kernel-modules})"
+  # systemd.services.rebuild = {
+  #   description = "Rebuilds and activates system config";
+  #   restartIfChanged = false;
+  #   path = [ pkgs.nixos-rebuild pkgs.systemd ];
+  #   script = ''
+  #     nixos-rebuild boot 
+  #     booted="$(readlink /run/booted-system/{initrd,kernel,kernel-modules})"
+  #     built="$(readlink /nix/var/nix/profiles/system/{initrd,kernel,kernel-modules})"
 
-      if [ "''${booted}" = "''${built}" ]; then
-        nixos-rebuild switch 
-      else
-        reboot now
-      fi
-    '';
-    serviceConfig.Type = "oneshot";
-  };
+  #     if [ "''${booted}" = "''${built}" ]; then
+  #       nixos-rebuild switch 
+  #     else
+  #       reboot now
+  #     fi
+  #   '';
+  #   serviceConfig.Type = "oneshot";
+  # };
 }
