43a7d9dac27b61b8de7fb4d8044f18b4f34a4877
hbakardzhiev <h.bakardzhiev@gmx.com>
2024-09-28 23:23:12 +0200

    [movedPomodoroToSystemAlice]

--- modules/homeManager/alice/timeManagement/pomodoro.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }: 
-{
-  # sops.secrets."pomodoro/start" = {
-  #   path = "/home/alice/.pomodoro/hooks/start";
-  #   mode = "7777";
-  # };
-  # home.packages = [ pkgs.libnotify ];
-}
