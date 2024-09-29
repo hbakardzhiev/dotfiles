6e2f901c641ba7e51ee36758938b75e9f1f2e988
hbakardzhiev <h.bakardzhiev@gmx.com>
2024-09-29 17:38:09 +0200

    [removeCommentedOutLines]

--- sops.nix
+++ sops.nix
@@ -19,26 +19,6 @@
       "ddns/duckdns/token" = {
         # owner = "alice";
       };
-      # "nextcloud/username" = {};
-      # "nextcloud/bobIP" = {};
-      # nextcloud = {
-      #   username = {};
-      #   bobIP = {};
-      #   # username = {};
-      #   password = {};
-      #   # bobIP = {};
-      # };
     };
-
-    # sops.defaultSopsFile = ./secrets/secrets.yaml;
-    # sops.defaultSopsFormat = "yaml";
-    # secrets.test = {
-    #   # sopsFile = ./secrets.yml.enc; # optionally define per-secret files
-
-    #   # %r gets replaced with a runtime directory, use %% to specify a '%'
-    #   # sign. Runtime dir is $XDG_RUNTIME_DIR on linux and $(getconf
-    #   # DARWIN_USER_TEMP_DIR) on darwin.
-    #   path = "%r/test.txt"; 
-    # };
-  };
+ };
 }
