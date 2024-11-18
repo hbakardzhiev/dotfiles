b5c806eec14a3fbfeb1d3d10570e59f86c7f13d1
hbakardzhiev <h.bakardzhiev@gmx.com>
2024-11-18 23:21:44 +0100

    []

--- flake.nix
+++ flake.nix
@@ -5,6 +5,10 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     # nixpkgs.url = "github:nixos/nixpkgs/2a0beb52294a22f28a2eb0c2a1d41aba088d6dfd";
     home-manager.url = "github:nix-community/home-manager";
+    nix-bitcoin = {
+      url = "github:fort-nix/nix-bitcoin/release";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nur.url = "github:nix-community/NUR";
     sops-nix = {
@@ -19,6 +23,7 @@
       home-manager,
       nur,
       sops-nix,
+      nix-bitcoin,
       ...
     }:
     let
@@ -74,9 +79,29 @@
           system = "x86_64-linux";
           specialArgs = {
             inherit sops-nix;
+            inherit nix-bitcoin;
           }; # Pass flake inputs to our config
           modules = [
             ./${maastricht}.nix
+            # nix-bitcoin.nixosModules.default
+            # {
+            #   # Automatically generate all secrets required by services.
+            #   # The secrets are stored in /etc/nix-bitcoin-secrets
+            #   nix-bitcoin.generateSecrets = true;
+
+            #   # Enable some services.
+            #   # See ../configuration.nix for all available features.
+            #   services.bitcoind.enable = true;
+            #   services.bitcoind.dataDir = "/run/media/et1";
+            #   services.clightning.enable = true;
+
+            #   # When using nix-bitcoin as part of a larger NixOS configuration, set the following to enable
+            #   # interactive access to nix-bitcoin features (like bitcoin-cli) for your system's main user
+            #   nix-bitcoin.operator = {
+            #     enable = true;
+            #     name = "alice";
+            #   };
+            # }
             home-manager.nixosModules.home-manager
             {
               home-manager.useGlobalPkgs = true;
