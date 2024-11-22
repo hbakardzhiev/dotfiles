fb5fa2c517da0f21c4e8a82c6e315e5c17ea5069
hbakardzhiev <h.bakardzhiev@gmx.com>
2024-11-22 23:23:09 +0100

    [btcNode]

---
+++ modules/system/bob/btc/node.nix
@@ -0,0 +1,33 @@
+{ ... }:
+{
+  imports = [
+    <nix-bitcoin/modules/presets/secure-node.nix>
+  ];
+
+  # Automatically generate all secrets required by services.
+  # The secrets are stored in /etc/nix-bitcoin-secrets
+  nix-bitcoin.generateSecrets = true;
+  nix-bitcoin.nodeinfo.enable = true;
+  # Add some backup
+  services.backups.enable = true;
+
+  services.spark-wallet.enable = true;
+  
+  # See ../configuration.nix for all available features.
+  services.bitcoind = {
+    enable = true;
+    listen = true;
+    # Add some pruning
+    prune = 100000;
+    dataDir = "/run/media/et1";              
+  };
+  
+  # Enable some services.
+  services.clightning.enable = true;
+  # When using nix-bitcoin as part of a larger NixOS configuration, set the following to enable
+  # interactive access to nix-bitcoin features (like bitcoin-cli) for your system's main user
+  nix-bitcoin.operator = {
+    enable = true;
+    name = "alice";
+  };
+}
