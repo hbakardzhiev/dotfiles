f408c88a76929031a9121046c4f2184178508c35
hbakardzhiev <h.bakardzhiev@gmx.com>
2025-01-15 02:29:16 +0100

    disabled av1 so that youtube only uses vp9 (which is hardware decoded)

--- modules/homeManager/alice/browsers/firefox.nix
+++ modules/homeManager/alice/browsers/firefox.nix
@@ -81,8 +81,10 @@
           "dom.battery.enabled" = false;
           # "media.video_stats.enabled" = false;
           # "media.ffmpeg.vaapi.enabled" = true;
-          # "media.hardware-video-decoding.enabled" = true;
-          # "media.hardware-video-decoding.force-enabled" = true;
+          "media.av1.enabled" = false;
+          "layers.acceleration.force-enabled" = true;
+          "media.hardware-video-decoding.enabled" = true;
+          "media.hardware-video-decoding.force-enabled" = true;
           "media.peerconnection.enabled" = true;
           "network.cookie.cookieBehavior" = 1;
           "network.http.referer.spoofSource" = true;
