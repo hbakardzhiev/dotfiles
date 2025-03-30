447eed9e314c5a066e29234e01806349a2a1a5e7
hbakardzhiev <h.bakardzhiev@gmx.com>
2025-03-30 11:43:55 +0200

    [syncthing]

--- modules/system/alice/sync/syncthing.nix
+++ modules/system/alice/sync/syncthing.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ ... }:
 let baseDir = "/drives/data/syncthing";
 in
 {
@@ -7,34 +7,5 @@ in
     configDir = "${baseDir}";
     user = "alice";
     group = "users";
-
-    # Override GUI and manual changes
-    overrideDevices = true;
-    overrideFolders = true;
-
-    settings = {
-      devices = {
-        "android" = {
-          id = config.sops.secrets."syncthing/pixel/id".path;
-        };
-        "laptop" = {
-          id = config.sops.secrets."syncthing/alice/id".path;
-        };
-      };
-
-      folders = {
-        "android-sync" = {
-          path = "${baseDir}/android-sync";
-          devices = [ "android" ];
-          type = "receiveonly";
-          ignoreDelete = true;
-        };
-      };
-
-      # Disable Anonymous Usage Reporting
-      options = {
-        urAccepted = 0;  # 0 or -1 disables usage reporting
-      };
-    };
   };
 }
