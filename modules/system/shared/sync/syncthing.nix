ece496b03208c4de6a4dc856c034cbd28ccb3095
hbakardzhiev <h.bakardzhiev@gmx.com>
2025-05-04 14:13:54 +0200

    [fixSyncthing]

--- modules/system/shared/sync/syncthing.nix
+++ modules/system/shared/sync/syncthing.nix
@@ -1,16 +1,17 @@
 { config, ... }:
-let hostname = config.networking.hostName;
-baseDir = if hostname == "alice"
-            then "/drives/data/syncthing"
-          else if hostname == "bob"
-            then "/home/alice/syncthing"
-          else "error";
 
+let hostname = config.networking.hostName;
+    baseDir =
+          if hostname == "alice"
+            then /drives/data/syncthing
+          else if hostname == "bob"
+            then /home/alice/syncthing
+          else "error";
 in
 {
   services.syncthing = {
     enable = true;
-    configDir = "${baseDir}";
+    configDir = baseDir;
     user = "alice";
     group = "users";
   };
