2e43e488de9edeb4124f709b977e09e8c9a2886a
hbakardzhiev <h.bakardzhiev@gmx.com>
2025-07-23 01:11:28 +0300

    [fixRsyncBackup]

--- modules/homeManager/eve/backup.nix
+++ modules/homeManager/eve/backup.nix
@@ -1,17 +1,24 @@
 { pkgs, ... }:
 {
-  home-manager.users.alice = {
-    systemd.user.services.foo = {
-      Unit = {
-        Description = "Backup configuration";
-      };
-      Service = {
-        ExecStart = "${pkgs.rsync}/bin/rsync -av --delete /run/media/et1/ /run/media/backup/ --exclude .git/";
-      };
-      Install = {
-        WantedBy = [ "timers.target" ];
-      };
-      startAt = "01:00";
+  systemd.user.services.foo = {
+    Unit = {
+      Description = "Backup configuration";
+    };
+    Service = {
+      ExecStart = "${pkgs.rsync}/bin/rsync -av --delete /run/media/et1/ /run/media/backup/ --exclude .git/";
+    };
+  };
+
+  systemd.user.timers.foo = {
+    Unit = {
+      Description = "Timer for backup service";
+    };
+    Timer = {
+      OnCalendar = "01:00:00";
+      Persistent = true;
+    };
+    Install = {
+      WantedBy = [ "timers.target" ];
     };
   };
 }
