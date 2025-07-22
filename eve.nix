44b5c5d08913ff2f011f9c0e3f6858335d8357c1
hbakardzhiev <h.bakardzhiev@gmx.com>
2025-07-22 12:01:14 +0300

    [addAutomaticNTFSfix]

--- eve.nix
+++ eve.nix
@@ -91,15 +91,42 @@ in
     };
   };
 
+
+  # Systemd service for /run/media/backup
+  systemd.services.fix-ntfs-backup = {
+    description = "Run ntfsfix on backup drive before mounting";
+    wantedBy = [ "multi-user.target" ];
+    before = [ "run-media-backup.mount" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.ntfs3g}/bin/ntfsfix /dev/disk/by-uuid/19F41D271DCA2DF1";
+      RemainAfterExit = true;
+      StandardOutput = "journal";
+      StandardError = "journal";
+    };
+  };
+  # Systemd service for /run/media/et1
+  systemd.services.fix-ntfs-et1 = {
+    description = "Run ntfsfix on et1 drive before mounting";
+    wantedBy = [ "multi-user.target" ];
+    before = [ "run-media-et1.mount" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.ntfs3g}/bin/ntfsfix /dev/disk/by-uuid/D01235C71235B378";
+      RemainAfterExit = true;
+      StandardOutput = "journal";
+      StandardError = "journal";
+    };
+  };
   # External strorage
   fileSystems."/run/media/backup" = {
     device = "/dev/disk/by-uuid/19F41D271DCA2DF1";
     fsType = "ntfs-3g";
-    options = [ "defaults" ];
+    options = [ "rw" "uid=1000" "nofail" ];
   };
   fileSystems."/run/media/et1" = {
     device = "/dev/disk/by-uuid/D01235C71235B378";
     fsType = "ntfs-3g";
-    options = [ "defaults" ];
+    options = [ "rw" "uid=1000" "nofail" ];
   };
 }
