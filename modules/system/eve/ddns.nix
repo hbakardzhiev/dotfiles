c62e04c9ec3297f589016d18dcaa7c1a150ac6e7
hbakardzhiev <h.bakardzhiev@gmx.com>
2025-07-25 09:02:06 +0300

    [moveToNixosDnsService]

---
+++ modules/system/eve/ddns.nix
@@ -0,0 +1,44 @@
+{
+  # pkgs,
+  config,
+  hostname,
+  ...
+}:
+let
+  # serviceName = "dynamic-dns-updater";
+  len = builtins.stringLength hostname;
+  # Extract the last character
+  lastChar = if len > 0 then builtins.substring (len - 1) 1 hostname else "";
+  fixedHost = hostname + lastChar + lastChar;
+  # token = config.sops.secrets."ddns/duckdns/token".path;
+  # curl = "${pkgs.curl}/bin/curl";
+  # updateDNS = pkgs.writeShellScript "updateDNS" ''
+  #   #!/usr/bin/env bash
+  #   output=$(<"${token}")
+  #   ${curl} "https://www.duckdns.org/update?domains=${fixedHost}&token=$output&ip="
+  # '';
+in
+{
+  services.duckdns = {
+    enable = true;
+    domains = [ fixedHost ];
+    tokenFile = config.sops.secrets."ddns/duckdns/token".path;
+  };
+  # systemd.user.timers.${serviceName} = {
+  #   Install.WantedBy = [ "timers.target" ];
+  #   Timer = {
+  #     OnBootSec = "1m";
+  #     OnUnitActiveSec = "30s";
+  #     Unit = "${serviceName}.service";
+  #   };
+  # };
+
+  # systemd.user.services.${serviceName} = {
+  #   Service = {
+  #     ExecStart = "${updateDNS}";
+  #   };
+  #   Install = {
+  #     WantedBy = [ "default.target" ];
+  #   };
+  # };
+}
