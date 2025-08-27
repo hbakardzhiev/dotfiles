64fe59f7592d3ae2bc6396d0962c60d896a25cae
hbakardzhiev <h.bakardzhiev@gmx.com>
2025-08-27 21:08:20 +0200

    [torBridgesWithSops]

--- modules/system/bob/btc/tor.nix
+++ modules/system/bob/btc/tor.nix
@@ -1,13 +1,12 @@
 {
   pkgs,
   config,
-  lib,
   ...
 }:
 {
   services.tor.settings = {
       UseBridges = true;
       ClientTransportPlugin = "obfs4 exec ${pkgs.obfs4}/bin/lyrebird";
-      Bridge = lib.splitString "\n" (builtins.readFile config.sops.secrets.tor_bridges.path);
+      Bridge = config.sops.secrets.tor_bridges;
   };
 }
