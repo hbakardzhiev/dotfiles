419ff8a7d9c56ff4138c9cf49143b19c1abf4c48
hbakardzhiev <h.bakardzhiev@gmx.com>
2025-08-30 01:26:23 +0200

    [disable]

--- modules/system/bob/ddns/duckdns.nix
+++ modules/system/bob/ddns/duckdns.nix
@@ -1,23 +1,24 @@
 { config, lib, ... }:
- let
-  cfg = config.cloud.ddns;
-in
-{
-  options.cloud.ddns = {
-    enable = lib.mkEnableOption "DDNS";
-    hostname = lib.mkOption {
-      type = lib.types.listOf lib.types.str;
-      description = "DuckDNS hostname to update";
-      default = [];
-    };
-  };
+{}
+#  let
+#   cfg = config.cloud.ddns;
+# in
+# {
+#   options.cloud.ddns = {
+#     enable = lib.mkEnableOption "DDNS";
+#     hostname = lib.mkOption {
+#       type = lib.types.listOf lib.types.str;
+#       description = "DuckDNS hostname to update";
+#       default = [];
+#     };
+#   };
 
-  config = { 
-    sops.secrets."ddns/duckdns/token" = { };
-    services.duckdns = {
-      enable = true;
-      domains = cfg.hostname;
-      tokenFile = config.sops.secrets."ddns/duckdns/token".path;
-    };
-  };
-}
+#   config = { 
+#     sops.secrets."ddns/duckdns/token" = { };
+#     services.duckdns = {
+#       enable = true;
+#       domains = cfg.hostname;
+#       tokenFile = config.sops.secrets."ddns/duckdns/token".path;
+#     };
+#   };
+# }
