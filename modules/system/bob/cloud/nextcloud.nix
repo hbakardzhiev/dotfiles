1c2714611a753d8ed6c91716d4a50690d4a574a3
hbakardzhiev <h.bakardzhiev@gmx.com>
2025-08-29 23:13:37 +0200

    refactor nix nextcloud option

--- modules/system/bob/cloud/nextcloud.nix
+++ modules/system/bob/cloud/nextcloud.nix
@@ -1,80 +1,69 @@
-{
-  pkgs,
-  config,
-  lib,
-  ...
-}:
+{ pkgs, config, lib, ... }:
+
+let
+  cfg = config.cloud.nextcloud;
+in
 {
   options.cloud.nextcloud = {
-    enable = lib.mkOption {
-      type = lib.types.bool;
-      description = "Enable Nextcloud service";
-      default = false;
-    };
+    enable = lib.mkEnableOption "Nextcloud";
     hostname = lib.mkOption {
       type = lib.types.str;
       description = "Nextcloud hostname";
       default = "";
     };
-
   };
-  config =
-    let
-      cfg = config.cloud.nextcloud;
-    in
-    lib.mkIf cfg.enable {
-      # Assert that variables that are needed are defined
-      assertions = [
-        {
-          assertion = cfg.hostname == "";
-          message = "hostname must be defined";
-        }
-      ];
 
-      # Secrets for Nextcloud
-      sops.secrets."nextcloud/username" = {
+  config = {
+    # Assertions for validation
+    assertions = lib.optionals cfg.enable [
+      {
+        assertion = cfg.hostname != "";
+        message = "cloud.nextcloud.hostname must be a non-empty string when cloud.nextcloud.enable is true.";
+      }
+    ];
+
+    # Enable Nextcloud only if cfg.enable is true
+    services.nextcloud = lib.mkIf cfg.enable {
+      enable = true;
+      package = pkgs.nextcloud31;
+      hostName = cfg.hostname;
+      https = true;
+      settings.overwriteprotocol = "https";
+      database.createLocally = true;
+      configureRedis = true;
+      maxUploadSize = "1G";
+      autoUpdateApps.enable = true;
+      extraAppsEnable = true;
+      extraApps = with config.services.nextcloud.package.packages.apps; {
+        inherit groupfolders;
+      };
+      config = {
+        adminuser = "root";
+        adminpassFile = config.sops.secrets."nextcloud/Password".path;
+      };
+      settings.enabledPreviewProviders = [
+        "OC\\Preview\\BMP"
+        "OC\\Preview\\GIF"
+        "OC\\Preview\\JPEG"
+        "OC\\Preview\\Krita"
+        "OC\\Preview\\MarkDown"
+        "OC\\Preview\\MP3"
+        "OC\\Preview\\OpenDocument"
+        "OC\\Preview\\PNG"
+        "OC\\Preview\\TXT"
+        "OC\\Preview\\XBitmap"
+        "OC\\Preview\\HEIC"
+      ];
+    };
+
+    # Secrets for Nextcloud
+    sops.secrets = lib.mkIf cfg.enable {
+      "nextcloud/username" = {
         owner = "alice";
       };
-      sops.secrets."nextcloud/Password" = {
+      "nextcloud/Password" = {
         owner = "alice";
-      };
-      # Nextcloud server config
-      services.nextcloud = {
-        enable = true;
-        package = pkgs.nextcloud31;
-        hostName = cfg.hostname;
-        https = true;
-        settings.overwriteprotocol = "https";
-        # Let NixOS install and configure the database automatically.
-        database.createLocally = true;
-        # Let NixOS install and configure Redis caching automatically.
-        configureRedis = true;
-        # Increase the maximum file upload size to avoid problems uploading videos.
-        maxUploadSize = "1G";
-        autoUpdateApps.enable = true;
-        extraAppsEnable = true;
-        extraApps = with config.services.nextcloud.package.packages.apps; {
-          # List of apps we want to install and are already packaged in
-          # https://github.com/NixOS/nixpkgs/blob/master/pkgs/servers/nextcloud/packages/nextcloud-apps.json
-          inherit groupfolders;
-        };
-        config = {
-          adminuser = "root";
-          adminpassFile = config.sops.secrets."nextcloud/Password".path;
-        };
-        settings.enabledPreviewProviders = [
-          "OC\\Preview\\BMP"
-          "OC\\Preview\\GIF"
-          "OC\\Preview\\JPEG"
-          "OC\\Preview\\Krita"
-          "OC\\Preview\\MarkDown"
-          "OC\\Preview\\MP3"
-          "OC\\Preview\\OpenDocument"
-          "OC\\Preview\\PNG"
-          "OC\\Preview\\TXT"
-          "OC\\Preview\\XBitmap"
-          "OC\\Preview\\HEIC"
-        ];
       };
     };
+  };
 }
