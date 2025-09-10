9bdccb7587c1da67cb2789b1288c8a5f914a6698
hbakardzhiev <h.bakardzhiev@gmx.com>
2025-09-10 21:23:39 +0200

    [nextcloudNotifyPush]

--- modules/system/shared/options/cloud/nextcloud.nix
+++ modules/system/shared/options/cloud/nextcloud.nix
@@ -72,6 +72,9 @@ in
           "OC\\Preview\\HEIC"
         ];
       };
+      notify_push = {
+        enable = true;
+      };
     };
     services.nginx.virtualHosts = {
       ${config.services.nextcloud.hostName} = {
