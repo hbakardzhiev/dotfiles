62b2e03262ad70fcdb8051c5938e3052ddfd0ca1
hbakardzhiev <h.bakardzhiev@gmx.com>
2025-10-20 20:00:33 +0200

    [nostrChromeExtension]

--- modules/homeManager/alice/browsers/brave.nix
+++ modules/homeManager/alice/browsers/brave.nix
@@ -21,7 +21,7 @@
       # { id = "blaaajhemilngeeffpbfkdjjoefldkok"; } # leechblock
       { id = "ifbmcpbgkhlpfcodhjhdbllhiaomkdej"; } # Office - Enable Copy and Paste (for word,excel...)
       { id = "nngceckbapebfimnlniiiahkandclblb"; } # Bitwarden
-      # { id = "kpgefcfmnafjgpblomihpgmejjdanjjp"; } # Nos2x
+      { id = "kpgefcfmnafjgpblomihpgmejjdanjjp"; } # Nos2x
       # { id = "cnjifjpddelmedmihgijeibhnjfabmlf"; } # Obsidian Web Clipper
     ];
     package = pkgs.brave;
