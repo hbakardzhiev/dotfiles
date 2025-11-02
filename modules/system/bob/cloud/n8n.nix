e6c092f65fb9f279317e0bc772efceb2cdeb1cba
hbakardzhiev <h.bakardzhiev@gmx.com>
2025-11-02 15:53:43 +0100

    [addCaddyServerForn8n]

--- modules/system/bob/cloud/n8n.nix
+++ modules/system/bob/cloud/n8n.nix
@@ -4,4 +4,12 @@
     enable = true;
     openFirewall = true;
   };
+  services.caddy = {
+    enable = true;
+    virtualHosts."n888n.v6.army" = {
+      extraConfig = ''
+        reverse_proxy 127.0.0.1:5678
+      '';
+    };
+  };
 }
