895b0574ab1fe3d675d554683020434a058a2dd0
hbakardzhiev <h.bakardzhiev@gmx.com>
2025-11-14 11:20:58 +0100

    move metabase endpoint to metabase url

--- modules/system/bob/cloud/metabase.nix
+++ modules/system/bob/cloud/metabase.nix
@@ -1,9 +1,19 @@
 { ... }:
+  let port = 8888;
+in 
 {
   services.metabase = {
     enable = true;
-    listen.port = 8888;
+    listen.port = port;
     openFirewall = true;
     ssl.enable = false;
+  };
+  services.caddy = {
+    enable = true;
+    virtualHosts."metabase.v6.army" = {
+      extraConfig = ''
+        reverse_proxy 127.0.0.1:${port}
+      '';
+    };
   };
 }
