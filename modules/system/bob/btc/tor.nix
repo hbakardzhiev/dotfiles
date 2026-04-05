a9689b94b58a9a9547491495fdd823a408efd8a4
hbakardzhiev <h.bakardzhiev@gmx.com>
2026-04-05 16:29:52 +0200

    disable bridge

--- modules/system/bob/btc/tor.nix
+++ modules/system/bob/btc/tor.nix
@@ -4,7 +4,7 @@
 }:
 {
   services.tor.settings = {
-    UseBridges = true;
+    UseBridges = false;
     ClientTransportPlugin = "obfs4 exec ${pkgs.obfs4}/bin/lyrebird";
     Bridge = [
       "obfs4 93.88.127.74:12345 12A047CCFBD804A421E679EFE7913E5F51ADEA43 cert=3OdJik4wWEbaOdPMBkInqgIPSFch3qSCTTDXfzEeabuVz9xkItxtR4OE3eUK9M/eS4ElVg iat-mode=0"
