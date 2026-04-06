4002380974f9559f27e6754eafcddf9994e29232
hbakardzhiev <h.bakardzhiev@gmx.com>
2026-04-06 11:15:18 +0200

    [bunker]

---
+++ modules/system/bob/nostr/bunker.nix
@@ -0,0 +1,74 @@
+{ config, pkgs, ... }:
+
+{
+  # === SOPS-NIX SETUP ===
+  sops = {
+    secrets = {
+      "nostr/nsec" = {
+        owner = "nak-bunker"; # we'll create this user
+        group = "nak-bunker";
+        mode = "0400"; # only readable by the service user
+      };
+    };
+  };
+
+  # === SYSTEMD SERVICE FOR NAK BUNKER ===
+  users.users.nak-bunker = {
+    isSystemUser = true;
+    group = "nak-bunker";
+    description = "Nostr nak bunker service user";
+  };
+
+  users.groups.nak-bunker = { };
+
+  systemd.services.nak-bunker = {
+    description = "Nostr NIP-46 Bunker (nak)";
+    wantedBy = [ "multi-user.target" ];
+    after = [
+      "network.target"
+      "tailscaled.service"
+    ];
+    requires = [ "tailscaled.service" ];
+
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = ''
+        ${pkgs.nak}/bin/nak bunker \
+          --sec ${config.sops.secrets."nostr/nsec".path} \
+          wss://nos.lol \
+          wss://nostr-01.uid.ovh \
+          wss://nostr-02.uid.ovh \
+          wss://nostr.mom \
+          wss://nostr.v6.army \
+          wss://nostr.wine \
+          wss://relay.camelus.app \
+          wss://relay.damus.io \
+          wss://relay.ditto.pub \
+          wss://relay.primal.net \
+          wss://relay.snort.social 
+      '';
+      Restart = "always";
+      RestartSec = "5s";
+
+      # Security hardening
+      User = "nak-bunker";
+      Group = "nak-bunker";
+      DynamicUser = false; # we use static user for secret access
+      StateDirectory = "nak-bunker";
+      WorkingDirectory = "/var/lib/nak-bunker";
+
+      # Strong sandboxing
+      ProtectSystem = "strict";
+      ProtectHome = true;
+      PrivateTmp = true;
+      PrivateDevices = true;
+      ProtectKernelTunables = true;
+      ProtectKernelModules = true;
+      ProtectControlGroups = true;
+      NoNewPrivileges = true;
+      RestrictRealtime = true;
+      RestrictNamespaces = true;
+      MemoryDenyWriteExecute = true;
+    };
+  };
+}
