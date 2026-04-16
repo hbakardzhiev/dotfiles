e75e820627f0964ff5fcc133fafe118f6757f069
hbakardzhiev <h.bakardzhiev@gmx.com>
2026-04-16 22:05:34 +0200

    [bobPowersave]

--- modules/system/bob/hardware-configuration.nix
+++ modules/system/bob/hardware-configuration.nix
@@ -43,4 +43,6 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  powerManagement.cpuFreqGovernor = lib.mkForce "powersave";
 }
