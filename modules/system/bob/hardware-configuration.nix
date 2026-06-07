e3f0c484efae2dfb80c6bb3e3a9edc67bc365371
hbakardzhiev <h.bakardzhiev@gmx.com>
2026-06-07 12:05:52 +0200

    [enableWifiBob]

--- modules/system/bob/hardware-configuration.nix
+++ modules/system/bob/hardware-configuration.nix
@@ -38,6 +38,9 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
+  networking = {
+    wireless = true;
+  };
   # networking.interfaces.enp2s0.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
 
