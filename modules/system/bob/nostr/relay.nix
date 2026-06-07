14e18a9fd294b8d1781850ce6b36e9ea353e75ca
hbakardzhiev <h.bakardzhiev@gmx.com>
2026-06-07 23:00:15 +0200

    [enableNostrRelayDebugging]

--- modules/system/bob/nostr/relay.nix
+++ modules/system/bob/nostr/relay.nix
@@ -3,10 +3,6 @@
   services.nostr-rs-relay = {
     enable = true;
     dataDir = "/var/lib/nostr-rs-relay";
-    # debugging
-    environment = {
-      RUST_LOG = "debug";
-    };
     settings = {
       info = {
         description = "Only I can publish events; open for reading.";
@@ -14,31 +10,30 @@
         relay_url = "wss://bobbb.duckdns.org";
         name = "bobbb.duckdns.org";
       };
-
       network = {
         # Bind to this network address
         address = "0.0.0.0";
         # Listen on port 12849 (this is the default). I have not managed to find any way to change it. KEEP IT default!
       };
-
       authorization = {
         nip42_auth = true;
         pubkey_whitelist = [
           "7f12a48deefa2b96f073bc2a21bf5a5c09580a2110801deaee1d0dba8d3135b9"
         ];
       };
-
       options = {
         max_event_size = 16384;
         reject_future_seconds = 1800;
       };
-
       limits = {
         max_subscriptions = 20;
         max_filters = 100;
       };
     };
-
+  };
+  # Inject the environment variable into the systemd unit
+  systemd.services.nostr-rs-relay.environment = {
+    RUST_LOG = "debug";
   };
 
   # Open firewall for the relay port
