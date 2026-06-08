7b34b59209f9b1a3376e39fd6bb7b88036c28d93
hbakardzhiev <h.bakardzhiev@gmx.com>
2026-06-08 20:56:30 +0200

    [enableSearx]

--- modules/system/bob/cloud/search.nix
+++ modules/system/bob/cloud/search.nix
@@ -1,21 +1,25 @@
 { ... }:
 {
-  # services.searx = {
-  # enable = true;
-  # settings = {
-  #   upstream_search_engines = {
-  #     DuckDuckGo = true;
-  #     Searx = true;
-  #     Brave = true;
-  #     Startpage = true;
-  #     LibreX = true;
-  #     Mojeek = true;
-  #     Bing = true;
-  #     Qwant = true;
-  #     Wikipedia = true;
-  #     Yahoo = true;
-  #     SepiaSearch = true;
-  #   };
-  # };
-  # };
+  services.searx = {
+    enable = true;
+    settings = {
+      server = {
+        port = 3456;
+        secret_key = "test123!";
+      };
+      #   upstream_search_engines = {
+      #     DuckDuckGo = true;
+      #     Searx = true;
+      #     Brave = true;
+      #     Startpage = true;
+      #     LibreX = true;
+      #     Mojeek = true;
+      #     Bing = true;
+      #     Qwant = true;
+      #     Wikipedia = true;
+      #     Yahoo = true;
+      #     SepiaSearch = true;
+      #   };
+    };
+  };
 }
