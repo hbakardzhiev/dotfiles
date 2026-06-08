1c7e6a83aa58f68b943b4742338ec12480e2c850
hbakardzhiev <h.bakardzhiev@gmx.com>
2026-06-08 20:35:12 +0200

    [stopSearch]

--- modules/system/bob/cloud/search.nix
+++ modules/system/bob/cloud/search.nix
@@ -1,21 +1,21 @@
 { ... }:
 {
-  services.searx = {
-    enable = true;
-    # settings = {
-    #   upstream_search_engines = {
-    #     DuckDuckGo = true;
-    #     Searx = true;
-    #     Brave = true;
-    #     Startpage = true;
-    #     LibreX = true;
-    #     Mojeek = true;
-    #     Bing = true;
-    #     Qwant = true;
-    #     Wikipedia = true;
-    #     Yahoo = true;
-    #     SepiaSearch = true;
-    #   };
-    # };
-  };
+  # services.searx = {
+  # enable = true;
+  # settings = {
+  #   upstream_search_engines = {
+  #     DuckDuckGo = true;
+  #     Searx = true;
+  #     Brave = true;
+  #     Startpage = true;
+  #     LibreX = true;
+  #     Mojeek = true;
+  #     Bing = true;
+  #     Qwant = true;
+  #     Wikipedia = true;
+  #     Yahoo = true;
+  #     SepiaSearch = true;
+  #   };
+  # };
+  # };
 }
