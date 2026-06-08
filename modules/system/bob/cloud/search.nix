218c56f3cd00ba92327540859cdd63ead4ca898d
hbakardzhiev <h.bakardzhiev@gmx.com>
2026-06-08 20:26:42 +0200

    [removeTempEngines]

--- modules/system/bob/cloud/search.nix
+++ modules/system/bob/cloud/search.nix
@@ -2,20 +2,20 @@
 {
   services.websurfx = {
     enable = true;
-    settings = {
-      upstream_search_engines = {
-        DuckDuckGo = true;
-        Searx = true;
-        Brave = true;
-        Startpage = true;
-        LibreX = true;
-        Mojeek = true;
-        Bing = true;
-        Qwant = true;
-        Wikipedia = true;
-        Yahoo = true;
-        SepiaSearch = true;
-      };
-    };
+    # settings = {
+    #   upstream_search_engines = {
+    #     DuckDuckGo = true;
+    #     Searx = true;
+    #     Brave = true;
+    #     Startpage = true;
+    #     LibreX = true;
+    #     Mojeek = true;
+    #     Bing = true;
+    #     Qwant = true;
+    #     Wikipedia = true;
+    #     Yahoo = true;
+    #     SepiaSearch = true;
+    #   };
+    # };
   };
 }
