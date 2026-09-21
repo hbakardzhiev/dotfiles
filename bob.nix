2035fe04943f2c229fd0fdfc1827704dea132081
hbakardzhiev <h.bakardzhiev@gmx.com>
2026-09-21 23:47:27 +0200

    disable bitcoin usb hdd

--- bob.nix
+++ bob.nix
@@ -86,10 +86,5 @@ in
       fsType = "ext4";
       options = [ "defaults" ];
     };
-    fileSystems."/run/media/bitcoin" = {
-      device = "/dev/disk/by-uuid/61a32c48-3bba-411c-a441-a7436652d7cd";
-      fsType = "ext4";
-      options = [ "defaults" ];
-    };
   };
 }
